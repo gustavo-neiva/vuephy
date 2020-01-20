88cd8705aa0cba8d86b26e20cccb359ea186ef97
Luiz Neiva <luizgzn@gmail.com>
2020-01-20 10:22:45 -0300

    update app.vue with components and remove vue's logo

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,69 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Vuephy'
-    }
-  }
-}
-</script>
 
 <style lang="scss">
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
 }
 
-h1, h2 {
-  font-weight: normal;
+.wrapper {
+  background: lightgrey;
+  display: grid;
+  grid-template-columns: 50% 50%;
 }
 
-ul {
-  list-style-type: none;
-  padding: 0;
+.first-column {
+  display: grid;
+  grid-template-rows: 10% 90%;
 }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
+#search-bar {
+  border: 1px solid #000;
 }
 
-a {
-  color: #42b983;
+#gif {
+  border: 1px solid #000;
 }
+
+#gif-list {
+  border: 1px solid #000;
+  height: 100%;
+}
+
 </style>
+
+<template>
+  <div class="wrapper" id="app">
+    <div class="first-column">
+      <div id="search-bar">
+        <SearchBar/>
+      </div>
+      <div id="gif">
+        <Gif/>
+      </div>
+    </div>
+    <div class="second-column">
+      <div id="gif-list">
+        <GifList/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Gif from './app/components/Gif.vue';
+import GifList from './app/components/GifList.vue';
+import SearchBar from './app/components/SearchBar.vue';
+
+export default {
+  name: 'app',
+  components: { Gif, GifList, SearchBar },
+  data () {
+    return {
+    }
+  }
+}
+</script>
