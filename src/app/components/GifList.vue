54b406a7f432a49a9f9cc803e36ed91ed951d3ce
Luiz Neiva <luizgzn@gmail.com>
2020-04-27 23:05:37 -0300

    adicionar lista de Gifs dentro do component Gifs

--- src/app/components/GifList.vue
+++ src/app/components/GifList.vue
@@ -1,18 +1,43 @@
 <style scoped>
+.container {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(320px, 1fr));
+  grid-gap: 1rem;
+  overflow-y: scroll;
+  grid-auto-rows: min-content;
+  height: 100vh;
+}
 
+.gif {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 </style>
 
 <template>
-  <div>
-    <h2>Gif List</h2>
-  </div>
+    <div class='container'>
+      <div v-for="gif in gifList" :key="gif.id">
+        <img class='gif' :src="gif.gifUrl()" alt="">
+      </div>
+    </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 
 export default {
     name: "GifList",
     data() {
+      return {
+        gifList: [],
+      }
+    },
+    computed: {
+      ...mapState('GifStore', ['gifs']),
+    },
+    mounted(){
+      this.gifList = this.gifs 
     }
 }
 </script>
