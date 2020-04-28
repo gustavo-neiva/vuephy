ca52d6b481f8a2c8ad6ed30e0a8cc6861dee8afc
Luiz Neiva <luizgzn@gmail.com>
2020-04-27 23:05:14 -0300

    adicionar objeto de dominio Gif dentro do componente Gif

--- src/app/components/Gif.vue
+++ src/app/components/Gif.vue
@@ -1,15 +1,33 @@
 <style>
 
+.gif {
+  width: 100%;
+  height: auto;
+  max-width: 70vh;
+}
+
 </style>
 
 <template>
-  <h2>Gif</h2>
+    <img class="gif" :src="gif.gifUrl()" alt="">
 </template>
 
 <script>
+
+import {mapState} from 'vuex'
+
 export default {
     name: "Gif",
     data() {
+      return {
+        gif: '',
+      } 
+    },
+    computed: {
+      ...mapState('GifStore', ['selectedGif']),
+    },
+    mounted() {
+      this.gif = this.selectedGif;
     }
 }
 </script>
