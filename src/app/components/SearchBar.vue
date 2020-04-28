38269143987358c817edb3cc0c3e62a73a82ffc7
Luiz Neiva <luizgzn@gmail.com>
2020-04-27 23:06:29 -0300

    adicionar debounce, estilo e emissao de dados pela search bar

--- src/app/components/SearchBar.vue
+++ src/app/components/SearchBar.vue
@@ -1,17 +1,42 @@
 <style scoped>
-
+ .search-bar {
+  height: 100%;
+  width: 100%;
+  display: block;
+  font-family: JosefinSans-Bold;
+  font-size: 8.4vh;
+  color: #1b3815;
+  line-height: 1.2vh;
+  outline: none;
+  border: none;
+ }
 </style>
 
 <template>
   <div>
-      <h2>SearchBar</h2>
+      <input v-model="query" @input="debounceInput" type="text" class="search-bar" placeholder="Search for Gifs">
   </div>
 </template>
 
 <script>
+import debounce from 'debounce';
+
 export default {
     name: "SearchBar",
     data() {
+      return {
+        query: '',
+      }
+    },
+    methods: {
+      debounceInput: debounce(function () {
+        if (this.query.length >= 3)
+          this.emitData();  
+        }, 400),
+
+      emitData(){
+        this.$emit('input', { query: this.query } );
+      },
     }
 }
 </script>
