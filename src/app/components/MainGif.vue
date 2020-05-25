3072adb54bcb61fa13e278fa4d02b753a1ff4f32
Luiz Neiva <luizgzn@gmail.com>
2020-05-25 00:54:48 -0300

    change main gif component name

---
+++ src/app/components/MainGif.vue
@@ -0,0 +1,38 @@
+<style>
+
+.gif {
+  width: 100%;
+  height: auto;
+  max-width: 70vh;
+}
+
+</style>
+
+<template>
+  <img class="gif" :src="gif.gifUrl()" alt="">
+</template>
+
+<script>
+
+import {mapState} from 'vuex'
+
+export default {
+  name: "MainGif",
+  data() {
+    return {
+      gif: '',
+    }
+  },
+  computed: {
+    ...mapState('GifStore', ['selectedGif']),
+  },
+  created() {
+    this.gif = this.selectedGif;
+  },
+  watch: {
+    selectedGif(newGif) {
+      this.gif = newGif;
+    }
+  }
+}
+</script>
