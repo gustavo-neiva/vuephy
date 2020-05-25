938ae4f408edb54af825921e2a7b1f0739c86575
Luiz Neiva <luizgzn@gmail.com>
2020-05-25 00:57:35 -0300

    increase gif limit from api

--- src/app/services/GiphyApi.js
+++ src/app/services/GiphyApi.js
@@ -1,11 +1,11 @@
 export default class GiphyApi {
   constructor(
-    api_key = "btrAmYnPogIdOxAztZfRL2JrUT7Y5Ofs", 
-    api_url = "https://api.giphy.com/v1/gifs/", 
-    type = "search", 
-    limit = 10, 
-    offset = 0, 
-    rating = "G", 
+    api_key = "btrAmYnPogIdOxAztZfRL2JrUT7Y5Ofs",
+    api_url = "https://api.giphy.com/v1/gifs/",
+    type = "search",
+    limit = 25,
+    offset = 0,
+    rating = "G",
     lang = "en"
     ) {
     this.api_url   = api_url;
@@ -20,11 +20,11 @@ export default class GiphyApi {
   mediaUrl(mediaId) {
     return `https://media1.giphy.com/media/${mediaId}/200.gif`;
   }
-  
+
   searchQueryUrl(query) {
     return `${this._apiUrl()}&q=${query}`
   }
-  
+
   _apiUrl() {
     return `${this.api_url}${this.type}?api_key=${this.api_key}&limit=${this.limit}&offset=${this.offset}&rating=${this.rating}&lang=${this.lang}`
   }
