76c67a0dfaf5fb2249875d210d435deab4d22115
Luiz Neiva <luizgzn@gmail.com>
2020-06-19 01:40:57 -0300

    clean up code

--- src/app/components/SelectedGif.vue
+++ src/app/components/SelectedGif.vue
@@ -1,4 +1,4 @@
-<style>
+<style scoped>
 
 .gif {
   width: 100%;
--- src/app/repositories/AllGifs.js
+++ src/app/repositories/AllGifs.js
@@ -10,19 +10,10 @@ export default class AllGifs {
 
   getBySearchQuery(query) {
     const queryUrl = this.api.searchQueryUrl(query)
-    console.log(queryUrl)
     return Axios.get(queryUrl)
     .then(({data}) => {
-      console.log(data.data)
       const Gifs = this.gifsFactory.createFromObjects(data.data)
       return Gifs
-    });
-  }
-
-  getGifMedia(gifId) {
-    return Axios.get(this.api.queryUrl(gifId))
-    .then(({data}) => {
-      console.log(data)
     });
   }
 
