d6cfd064211bc0da9b2f552723d7303c7a98cf42
Luiz Neiva <luizgzn@gmail.com>
2020-06-21 02:28:19 -0300

    add lazyload plugin

--- src/main.js
+++ src/main.js
@@ -2,7 +2,14 @@ import Vue from 'vue'
 import store from './vuex/mainStore';
 import App from './App.vue';
 import VueMasonry from 'vue-masonry-css'
+import VueLazyload from 'vue-lazyload'
 
+Vue.use(VueLazyload, {
+  preLoad: 1.3,
+  error: 'dist/error.png',
+  loading: 'dist/loading.gif',
+  attempt: 1
+})
 Vue.use(VueMasonry);
 
 new Vue({
