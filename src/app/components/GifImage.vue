10a91348229e3ec854cc2eff6ae4e10baece3edf
Luiz Neiva <luizgzn@gmail.com>
2020-06-21 02:28:49 -0300

    add an image component for lazy loading

---
+++ src/app/components/GifImage.vue
@@ -0,0 +1,25 @@
+<style scoped>
+.gif {
+  height: auto;
+  width: 100%;
+}
+</style>
+<template>
+  <div>
+    <img
+      class="gif"
+      v-lazy="source"
+    >
+  </div>
+</template>
+<script>
+export default {
+  name: "GifImage",
+  props: {
+    source: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
